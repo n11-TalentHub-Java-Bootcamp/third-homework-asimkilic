2ccbd9445f86b8408c419f95e557b77619808267
asimkilic <a.asim.kilic@gmail.com>
2021-12-23 14:08:02 +0300

    project created

---
+++ src/main/java/com/asimkilic/mongodbhw3/MongodbHw3Application.java
@@ -0,0 +1,13 @@
+package com.asimkilic.mongodbhw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbHw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbHw3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/asimkilic/mongodbhw3/MongodbHw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asimkilic.mongodbhw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbHw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
